5a6230390cf28e0bb69edc5cf8ffcf74f7d06208
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-05-04 20:31:34 -0300

    feat: added image download logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,12 +1,14 @@
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const googleSearchCredentials = require('./../credentials/google-search.json')
+const imageDownloader = require('image-downloader')
 
 const state = require('./state.js')
 
 async function robot(){
     const content = state.Load()
-    await fetchImagesOfAllSentences()
+    //await fetchImagesOfAllSentences()
+    await downloadAllImages()
     state.Save(content)
 
     async function fetchImagesOfAllSentences(){
@@ -30,6 +32,36 @@ async function robot(){
         const imageUrl = response.data.items.map( item => item.link )
 
         return imageUrl
+    }
+
+    async function downloadAllImages(){
+        content.downloadedImages = []
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            const images = content.sentences[sentenceIndex].images
+
+            for (const imageUrl of images){
+                
+                try {
+                    if (content.downloadedImages.includes(imageUrl)){
+                        throw new Error('> Error: image already downloaded')
+                    }
+
+                    await downloadImageAndSave(imageUrl, `${sentenceIndex}-original.png`)                    
+                    content.downloadedImages.push(imageUrl)
+                    break
+                } catch (error) {
+                    console.log(`> error to download the image. ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadImageAndSave(imageUrl, fileName){
+        return imageDownloader.image({
+            url: imageUrl,
+            dest: `./content/${fileName}`
+        })
     }
 
 }
